2c9a36c82ea71c5abf05628f7159e7a7096c69f5
SergeyPotapov01 <88240890+SergeyPotapov01@users.noreply.github.com>
2021-10-09 17:21:18 +0500

    Version 1.1.2
    1 - Настроил логирование
    2 - Небольшие доработки

--- IrisCoinFarm.py
+++ IrisCoinFarm.py
@@ -1,5 +1,4 @@
 import asyncio
-import datetime
 
 from random import randint
 
@@ -8,10 +7,16 @@ from DataBase import DataBase
 import requests
 import vk_api
 
+from  loguru import logger
 
-__version__ = '1.1.1'
+
+__version__ = '1.1.2'
+
+logger.add('log/irisCoinFarm.log', format='{time} {level} {message}',
+           level='INFO', rotation='1 week', compression='zip')
 
 dataBase = DataBase()
+
 
 async def _asyncIrisCoinFarm(access_token):
     while True:
@@ -20,15 +25,15 @@ async def _asyncIrisCoinFarm(access_token):
         try:
             vk.wall.createComment(owner_id=-174105461, post_id=6713149, message='Ферма')
             id = str(vk.users.get()[0]['id'])
-            print(f'Комментарий на фарм создан с id{id} в ' + str(datetime.datetime.today()))
+            logger.info(f'Комментарий на фарм создан с id{id}')
         except vk_api.exceptions.ApiError:
             dataBase.deleteInvalidToken(access_token)
-            print(f'Токен инвалид надо переделать{access_token}')
+            logger.error(f'Токен не действительный {access_token}')
         except requests.exceptions.ConnectionError:
-            print('Проблема с тынтырнетом надо будет это переписать, а то как не кайф')
-            await asyncio.sleep(10)
+            logger.error('Проблема с подключением к сети Интеррнет')
+            await asyncio.sleep(30)
             continue
-        await asyncio.sleep(60*60*4 + randint(180, 420))
+        await asyncio.sleep(60*60*4 + randint(120, 420))
 
 
 async def asyncIrisCoinFarm(dataBase: DataBase):
@@ -40,34 +45,45 @@ async def asyncIrisCoinFarm(dataBase: DataBase):
 async def asyncUX(loop):
     while True:
         enteredQuery = await loop.run_in_executor(None, input, 'Введите цифру:\n 1 - Добавить аккаунт по паролю \n 2 - Добавить токен в БД(если токен инвалид, то его дропнет с бд)\n 3 - Показать аккаунты загруженные в бд \n 4 - Закрыть программу \n')
+
         if enteredQuery == '1':
             login = input('Введи логин: ')
             password = input('Введи пароль: ')
+
             try:
                 dataBase.getTokenAndSendToDB(login=login, password=password)
             except vk_api.exceptions.BadPassword:
-                print('Неверно введен логин либо пароль')
+                logger.error('Неверно введен логин либо пароль')
+            except vk_api.exceptions.LoginRequired:
+                logger.error('Введена пустая строка логина')
+            except requests.exceptions.ConnectionError:
+                logger.error('Отсутствует логин')
+            except vk_api.exceptions.PasswordRequired:
+                logger.error('Отсутствует пароль')
+                
         elif enteredQuery == '2':
             token = input('Введи токен: ')
             dataBase.sendTokenToDB(token)
         elif enteredQuery == '3':
             for token in dataBase.sendID():
-                print(token[0])
+                logger.info('в БД присутствует id'+str(token[0]))
         elif enteredQuery == '4':
-            print('Закрытие программы')
+            logger.info('Закрытие программы')
             loop.stop()
             break
         else:
-            print('Ты ввел какую то хуйню, введи еще раз но без пробелов')
+            logger.error(f'%{enteredQuery}% - Не корректный запрос, попробуй еще раз')
 
 async def main():
     loop = asyncio.get_event_loop()
     await asyncio.gather(
-        asyncIrisCoinFarm(dataBase),
         asyncUX(loop),
+        asyncIrisCoinFarm(dataBase),
     )
 
-try:
-    asyncio.run(main())
-except RuntimeError:
-    print('Event Loop остановлен')
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except RuntimeError:
+        logger.info('Event Loop остановлен')
